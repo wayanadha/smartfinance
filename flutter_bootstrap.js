2ff7be0c35249c08a63889b7d0f883b802a5d316
wayanadha <wayan.adha07@gmail.com>
2025-12-30 02:02:22 +0700

    Add lessons feature with user approval system

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1155085477"
+    serviceWorkerVersion: "4196178153"
   }
 });
